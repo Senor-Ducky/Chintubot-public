09c6591a7b4183d2f9ee864635515806d905031b
DoctorEpsilon <Priyanshgoel05@gmail.com>
2020-12-22 22:21:49 +0530

    Update Fun.py

--- cogs/Fun.py
+++ cogs/Fun.py
@@ -11,11 +11,6 @@ class Fun(commands.Cog):
     async def on_ready(self):
         print(f"{type(self).__name__} Cog ready.")
 
-    @commands.command()
-    async def say(self, ctx: commands.Context):
-        message = f"""The help command is being worked!
-        If you have any questions please feel free to dm my owner"""
-        await ctx.send(embed=system_embed(ctx.author.role, message))
 
 
 def setup(bot):
