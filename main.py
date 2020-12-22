63ba2c86e5df0a58cbd14d96c5bf7cf9067dfcc2
programmergaurav <gauravgupta14376@gmail.com>
2020-12-22 19:44:40 +0530

    Added Warn Command

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import discord
-client = discord.Client()
+from discord.ext import commands
+from pymongo import MongoClient
+
+bot = commands.Bot(command_prefix='$')
 
 
-client.run('NzkwOTAwOTUwODg1MjAzOTc4.X-HV6A.VBQd4nfGOFXSkYdDvmBBGXn-aiw')
+@bot.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(bot))
+
+
+@bot.command()
+async def warn(ctx, member, *reason):
+    print(reason)
+    embed = discord.Embed(
+        description=str(member + " is warned | Reason = " + " ".join(reason)),
+        colour=discord.Colour.blue()
+    )
+    await ctx.send(embed=embed)
+
+bot.run('NzkwOTAwOTUwODg1MjAzOTc4.X-HV6A.VBQd4nfGOFXSkYdDvmBBGXn-aiw')
