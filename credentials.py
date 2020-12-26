a58653e60209a7ae62bb27ad0dfe4fce45f775ab
Yuvraj.M <63858962+YuvrajGeek@users.noreply.github.com>
2020-12-26 21:49:22 +0530

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,9 +1,1 @@
-CREDENTIALS = {
-    'DISCORD_TOKEN': 'NzkxNjgzMTM2NjEwMTA3NDM0.X-SuYA.NNz3HW75Ogy1kTnYzBMC2JkqLFY',
-    'REDDIT_API_CLIENT_ID': 'e8gkJUAQLc5t0Q',
-    'REDDIT_API_CLIENT_SECRET': 'Mcod2NteR-Nf7CRpHx5tFX4yzon52g',
-    #'RAPID_API_KEY': ''
-}
 
-def get_credentials(key):
-	return CREDENTIALS[str(key)]
