6bdf7c219fac7f06c1636bb0ef2eb68d2966d3bf
YuvrajGeek <yuvrajmotiramani5115@gmail.com>
2020-12-26 21:58:23 +0530

    added suggestion and image commands

--- credentials.py
+++ credentials.py
@@ -1,1 +1,9 @@
+CREDENTIALS = {
+    'DISCORD_TOKEN': 'NzkxNjgzMTM2NjEwMTA3NDM0.X-SuYA.AkzhHsstBQP8HyXGx7yTRgMP8pE',
+    'REDDIT_API_CLIENT_ID': 'e8gkJUAQLc5t0Q',
+    'REDDIT_API_CLIENT_SECRET': 'Mcod2NteR-Nf7CRpHx5tFX4yzon52g',
+    #'RAPID_API_KEY': ''
+}
 
+def get_credentials(key):
+	return CREDENTIALS[key]
