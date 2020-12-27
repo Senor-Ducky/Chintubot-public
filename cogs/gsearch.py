ad3e5562f695c72e9d89ba4dc1bf5cc9b8e8f212
Devansh-bit <67866743+Devansh-bit@users.noreply.github.com>
2020-12-28 01:01:08 +0530

    Removed the requirement of double quotes for gsearch.

--- cogs/gsearch.py
+++ cogs/gsearch.py
@@ -8,7 +8,15 @@ class Google(commands.Cog):
         self.commands = commands
 
     @commands.command()
-    async def gsearch(self, ctx, search_string, number_of_results=2):
+    async def gsearch(self, ctx):
+        message = str(ctx.message.content).split()
+        try:
+            number_of_results = int(message[-1])
+            search_string = " ".join(message[1:-1])
+        except ValueError:
+            number_of_results = 2
+            search_string = " ".join(message[1:])
+        print(search_string)
         embed = discord.Embed(title=search_string, color=discord.Color.blue())
         i = 0
         for result in search(search_string, tld='com', lang='en', start=0, stop=number_of_results):
